32e4fb899c83b0aa3f85ba6de216081ebbee3151
Daewoo, Kim <bighouse.kim@gmail.com>
2014-12-18 00:30:26 +0900

    - 초기 임포트.

---
+++ vagrant/install_ansible.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y python-software-properties
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible

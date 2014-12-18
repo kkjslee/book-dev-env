e2d1d13d9d6505ac1cf2e040a5de510676eef0a0
dwkim <dwkim@delphigames.com>
2014-12-18 17:00:21 +0900

    - ansigle Redis 설정 템플릿 파일 추가.

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -1,11 +1,11 @@
-#apt-get update
-#apt-get install -y git
-#apt-get install -y python-software-properties
-#apt-add-repository -y ppa:ansible/ansible
-#apt-get update
-#apt-get install -y ansible
-#cd /home/vagrant
-#git clone http://github.com/lekdw/book-dev-env.git
-#chown -R vagrant:vagrant book-dev-env
+apt-get update
+apt-get install -y git
+apt-get install -y python-software-properties
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+cd /home/vagrant
+git clone http://github.com/lekdw/book-dev-env.git
+chown -R vagrant:vagrant book-dev-env
 cd book-dev-env/ansible
 ansible-playbook -i hosts site.yml
